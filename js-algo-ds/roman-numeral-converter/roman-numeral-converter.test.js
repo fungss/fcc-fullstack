feb135022addad1ef24bfb0cd9b37b54b47512fd
ronald.fung <fungssronald@gmail.com>
2022-04-02 21:12:45 +0800

    Clean up

--- js-algo-ds/roman-numeral-converter/roman-numeral-converter.test.js
+++ js-algo-ds/roman-numeral-converter/roman-numeral-converter.test.js
@@ -141,15 +141,3 @@ test.each(tests)(
         expect(convertToRoman(input)).toBe(expected);
     }
 );
-
-
-// test
-// var testList = [2, 3, 4, 5, 9, 12, 16, 29, 44, 45, 68, 83, 97, 99, 400, 500, 501, 649, 798, 891, 1000, 1004, 1006, 1023, 2014, 3999]
-// testList.forEach(
-//     (num) => {
-//         console.log(num+": "+convertToRoman(num));
-//     }
-// );
-
-// to-do
-// test with jest
